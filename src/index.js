80a8fe5d2c8ac1ca06afc2e42b35bc2f37c4bcdc
Thiago Neres <thiago.tnsb@gmail.com>
2018-09-28 08:15:44 -0300

    project automatized, with create-react-app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './img/favicon.ico';
+import './css/index.css';
+import './css/pure-min.css';
+import App from './components/App.jsx'
+
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#main")
+)
