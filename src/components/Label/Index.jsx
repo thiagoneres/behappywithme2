0ced9b988050dc0a6f2c4b1b536a6f7913f546f3
Thiago Neres <thiago.tnsb@gmail.com>
2018-10-01 09:15:16 -0300

    Create component Label for form NovoUsuario

--- src/components/Label/Index.jsx
+++ src/components/Label/Index.jsx
@@ -1,5 +1,7 @@
 import react from 'react'
 
-export default function Label (props){
-
+export default function Label (props){ //para passar o parâmetro props através de atributo ao component (<Label atributo="xxx">)
+    return(
+        <label for={props.for}>{props.texto}</label> //O atributo for é	 um	 recurso que auxilia no clique do componente associado.
+    );
 }
