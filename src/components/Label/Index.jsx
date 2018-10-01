2c8cf6057a35291967400cd3357c3e1862e4abc3
Thiago Neres <thiago.tnsb@gmail.com>
2018-10-01 10:01:48 -0300

    Style components of form

--- src/components/Label/Index.jsx
+++ src/components/Label/Index.jsx
@@ -1,7 +1,15 @@
-import react from 'react'
+import React from 'react'
 
 export default function Label (props){ //para passar o parâmetro props através de atributo ao component (<Label atributo="xxx">)
+    const estilo = {
+        color: props.valorInvalido ? '#d50000' : 'ff0000' // if ternário
+    }
+
     return(
-        <label for={props.for}>{props.texto}</label> //O atributo for é	 um	 recurso que auxilia no clique do componente associado.
+        <label
+            style={estilo}
+            htmlFor={props.htmlFor}>
+            {props.texto}
+        </label> //O atributo for é	 um	 recurso que auxilia no clique do componente associado.
     );
 }
--- src/components/NovoUsuario/index.jsx
+++ src/components/NovoUsuario/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Label from '../Label/Index.jsx'
 
 //definimos o component como classe, pois nele serão armazenados dados(states)
+//O valor da propriedade htmlFor ficou como nome,porque este será	o id da	<input>	associada.
+//classes do pure.css --> pure-form(define o estilo básico do form) e o pure-form-stacked(posiciona uma tag abaixo da outra)
 class NovoUsuario extends React.Component{
     render(){
         return(
             <div className="center">
+                <form className="pure-form pure-form-stacked">
+                    <Label htmlFor="nome" texto="Quem é você?"></Label> 
+                </form>
             </div>
 
         )
