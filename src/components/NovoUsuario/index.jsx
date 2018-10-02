d14299ba7a872e1b1d6e095b24b710f1ed74db24
Thiago Neres <thiago.tnsb@gmail.com>
2018-10-02 08:58:38 -0300

    Update NovoUsuario

--- src/components/NovoUsuario/index.jsx
+++ src/components/NovoUsuario/index.jsx
@@ -1,23 +1,50 @@
 import React from 'react'
 import Label from '../Label/Index.jsx'
-import	Input	from	'../Input/Index.jsx'
+import Input from '../Input/Index.jsx'
 
 //definimos o component como classe, pois nele serão armazenados dados(states)
 //O valor da propriedade htmlFor ficou como nome,porque este será	o id da	<input>	associada.
 //classes do pure.css --> pure-form(define o estilo básico do form) e o pure-form-stacked(posiciona uma tag abaixo da outra)
-class NovoUsuario extends React.Component{
-    render(){
-        return(
+class NovoUsuario extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            usuario: {
+                nome: ''
+            },
+            validacao: {
+                nomeInvalido: false
+            }
+        };
+
+    }
+
+    atualizarNome(e) {
+        let usuario = this.state.usuario;
+        usuario.nome = e.target.value;
+        this.setState({
+            usuario: usuario
+        });
+    }
+
+    render() {
+        return (
             <div className="center">
                 <form className="pure-form pure-form-stacked">
-                    <Label htmlFor="nome" texto="Quem é você?"></Label> 
+                    <Label
+                        htmlFor="nome"
+                        texto="Quem	é você?"
+                        valorInvalido={this.state.validacao.nomeInvalido}
+                    />
                     <Input
                         id="nome"
-                        placeholder="Digite	seu	nome"
+                        placeholder="Digite seu nome"
                         maxLength="40"
-                        readOnly
-                        valorInvalido
-                        defaultValue="Guilherme"
+                        readOnly={false}
+                        valorInvalido={this.state.validacao.nomeInvalido}
+                        defaultValue={this.state.usuario.nome}
+                        onChange={this.atualizarNome.bind(this)}
                     />
                 </form>
             </div>
