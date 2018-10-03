8835fc3cbfa9fec23029c6b8fd37314350d23db2
Thiago Neres <thiago.tnsb@gmail.com>
2018-10-03 14:18:01 -0300

    It also concludes the adjustments of the structure of App and NewUsuario components with field validation and Toast

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './Header';
 import NovoUsuario from './NovoUsuario';
+import Toast from './Toast/Index.jsx';
 
-class App extends React.Component{
-    render(){
-        return(
+class App extends React.Component {
+    render() {
+        return (
             <div>
-                <Header/>
-                <NovoUsuario/>
+                <Header />
+                <NovoUsuario erro={msg => this.refs.toast.erro(msg)} />
+                <Toast ref="toast" />
             </div>
         );
     }
