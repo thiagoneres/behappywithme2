9decded72c40400911aedb8b2a8e8695bcf30bb0
Thiago Neres <thiago.tnsb@gmail.com>
2018-10-03 14:40:21 -0300

    Completed ButtonImage component

---
+++ src/components/ButtonImage/Index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Image from '../Image/Index.jsx'
+import './Index.css'
+
+export default function ButtonImage(props) {
+    let estilo = {};
+    let index = 0;
+    if (props.posicao === 'direita') {
+        estilo.float = 'right'
+        index = 1
+    } else {
+        estilo.float = 'left'
+        index = 0
+    }
+    const tamanho = 30;
+
+    let propriedades = Object.assign({}, props);
+    delete propriedades.posicao;
+
+    return (
+        <div
+            style={estilo}
+            className='option-image-scroller'
+            {...propriedades}
+        >
+            <Image
+                eixoX={index}
+                eixoY={0}
+                width={tamanho}
+                height={tamanho}
+                backgroundHeight={tamanho}
+                arquivo="img/botoes.png"
+            />
+        </div>
+    )
+
+}
